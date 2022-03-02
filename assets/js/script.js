aaf6bb8b6234e8203882e1ec956395a93c638277
CassDoes <c.doexo@gmail.com>
2022-03-02 16:13:07 -0600

    updated js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,63 @@
 var timerEl = document.getElementById("timer");
-var introductionEl = document.querySelector(".introduction");
-var quizEl = document.querySelector(".quiz-form");
 var startButtonEl = document.querySelector("#start-quiz");
-var quizNumEl = document.querySelector("#quiz-number");
-var questionEl = document.querySelector("#quiz-question");
+var quizEl = document.querySelector(".quiz-form")
 
-quizEl.hidden = true;
+//code2.0 elements
+const falseBtnEl = document.getElementById("False");
+const trueBtnEl = document.getElementById("True");
+const questionEl = document.getElementById("question-text");
+const quizNumTextEl = document.getElementById("quiz-number");
+const introductionEl = document.querySelector(".introduction");
 
-var startQuiz = function() {
-    introductionEl.hidden = true;
-    quizEl.hidden = false;
+quizEl.classList.add("hide");
 
-    var answerEl = document.querySelector("#quiz-question");
-    for (var i = 0 ; i < list.length; i++) {
-        questionEl.textContent = list[i].question;
+let currentQuestion = 0;
 
-        var options = list[i].choices;
-        for (var opt in options) {
-            var btn = document.createElement("button")
-            btn.type = "submit";
-            btn.textContent = options[opt];
-            answerEl.appendChild(btn);
+
+function startQuiz() {
+    introductionEl.classList.add("hide");
+    quizEl.classList.remove("hide");
+    currentQuestion = 0;
+
+    quizNumTextEl.innerHTML = "Question " + list.length;
+    questionEl.innerHTML = list[currentQuestion].question;
+    trueBtnEl.innerHTML = list[currentQuestion].answers[0].option;
+
+    trueBtnEl.onclick = () => {
+        if (currentQuestion < 5) {
+            next();
+        }
+    }
+
+    falseBtnEl.innerHTML = list[currentQuestion].answers[1].option;
+
+    falseBtnEl.onclick = () => {
+        if (currentQuestion < 5) {
+            next();
+        }
+    }
+
+    startQuiz();
+
+    function next() {
+        currentQuestion++;
+
+        quizNumTextEl.innerHTML = "Question " + list.length;
+        questionEl.innerHTML = list[currentQuestion].question;
+        trueBtnEl.innerHTML = list[currentQuestion].answers[0].option;
+
+        trueBtnEl.onclick = () => {
+        if (currentQuestion < 5) {
+            next();
+        }
+    }
+      
+        falseBtnEl.innerHTML = list[currentQuestion].answers[1].option;
+
+        falseBtnEl.onclick = () => {
+            if (currentQuestion < 5) {
+            next();
+            }
         }
     }
 };
@@ -41,36 +78,55 @@ function countdown() {
   }, 1000);
 }
 
-var list =
-    [
-        {
-            question: "question one",
-            choices: ["one", "two", "three", "four"],
-            answer: 2
-        },
-        {
-            question: "question two",
-            choices: ["one", "two", "three", "four"],
-            answer: 4
-        },
-        {
-            question: "question three",
-            choices: ["one", "two", "three", "four"],
-            answer: 1
-        },
-        {
-            question: "question four",
-            choices: ["one", "two", "three", "four"],
-            answer: 2
-        },
-        {
-            question: "question five",
-            choices: ["one", "two", "three", "four"],
-            answer: 3
-        }
-    ];
+let list = [
+    {
+        question: "question one",
+        answers: [
+            {option: "one", answer: true},
+            {option: "two", answer: false},
+            {option: "three", answer: false},
+            {option: "four", answer: false},
+        ]
+    },
+    {
+        question: "question two",
+        answers: [
+            {option: "one", answer: false},
+            {option: "two", answer: false},
+            {option: "three", answer: true},
+            {option: "four", answer: false},
+        ]
+    },
+    {
+        question: "question three",
+        answers: [
+            {option: "one", answer: false},
+            {option: "two", answer: true},
+            {option: "three", answer: false},
+            {option: "four", answer: false},
+        ]
+    },
+    {
+        question: "question four",
+        answers: [
+            {option: "one", answer: false},
+            {option: "two", answer: true},
+            {option: "three", answer: false},
+            {option: "four", answer: false},
+        ]
+    },
+    {
+        question: "question five",
+        answers: [
+            {option: "one", answer: false},
+            {option: "two", answer: false},
+            {option: "three", answer: false},
+            {option: "four", answer: true},
+        ]
+    }
+];
+
 
 
 startButtonEl.addEventListener("click", startQuiz);
-startButtonEl.addEventListener("click", countdown)
-
+startButtonEl.addEventListener("click", countdown);
